826bd2eb605f3fd3fce0c2e1045515e85f7d2e8a
Ke Su <nagizero@ALTEREGO.localdomain>
2018-03-07 18:27:05 +0800

    minor

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import utils
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--epochs', type=int, default=60)
     parser.add_argument('--num_hid', type=int, default=1024)
     parser.add_argument('--model', type=str, default='baseline0_newatt')
     parser.add_argument('--output', type=str, default='saved_models/exp0')
