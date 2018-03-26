852f426b31a98592760af90f192cac85dc637f0e
nagizeroiw <nagizero@foxmail.com>
2018-03-26 14:38:35 +0800

    minor

--- base_model.py
+++ base_model.py
@@ -27,13 +27,14 @@ class BaseModel(nn.Module):
         return: logits, not probs
         """
 
-        batch, _, num_objs, obj_dim = v.size()
-        _, __, ___, b_dim = b.size()
-        _, __, seq_length = q.size()
+        if v.dim() == 4:  # handle pair loss
+            batch, _, num_objs, obj_dim = v.size()
+            _, __, ___, b_dim = b.size()
+            _, __, seq_length = q.size()
 
-        v = v.view(-1, num_objs, obj_dim)  # (2 * batch, num_objs, obj_dim)
-        b = b.view(-1, num_objs, b_dim)  # (2 * batch, num_objs, b_dim)
-        q = q.view(-1, seq_length)  # (2 * batch, seq_length)
+            v = v.view(-1, num_objs, obj_dim)  # (2 * batch, num_objs, obj_dim)
+            b = b.view(-1, num_objs, b_dim)  # (2 * batch, num_objs, b_dim)
+            q = q.view(-1, seq_length)  # (2 * batch, seq_length)
 
         '''
         if not self.seen_back2normal_shape:
@@ -53,48 +54,6 @@ class BaseModel(nn.Module):
         v_repr = self.v_net(v_emb)  # image representation [batch, num_hid]
         joint_repr = q_repr * v_repr  # joint embedding (joint representation) [batch, num_hid]
         logits = self.classifier(joint_repr)  # answer (answer probabilities) [batch, n_answers]
-        return logits
-
-
-class BaseModelWithPairLoss(nn.Module):
-    def __init__(self, w_emb, q_emb, v_att, q_net, v_net, classifier):
-        super(BaseModel, self).__init__()
-        self.w_emb = w_emb
-        self.q_emb = q_emb
-        self.v_att = v_att
-        self.q_net = q_net
-        self.v_net = v_net
-        self.classifier = classifier
-        self.seen_back2normal_shape = False
-
-    def forward(self, v, b, q, labels):
-        """Forward
-
-        v: [batch, 2, num_objs, obj_dim]
-        b: [batch, 2, num_objs, b_dim]
-        q: [batch, 2, seq_length]
-
-        return: logits, not probs
-        """
-
-        batch, _, num_objs, obj_dim = v.size()
-        _, __, ___, b_dim = b.size()
-        _, __, seq_length = q.size()
-
-        v = v.view(-1, num_objs, obj_dim)  # (2 * batch, num_objs, obj_dim)
-        b = b.view(-1, num_objs, b_dim)  # (2 * batch, num_objs, b_dim)
-        q = q.view(-1, seq_length)  # (2 * batch, seq_length)
-
-        w_emb = self.w_emb(q)  # preprocess question [2 * batch, seq_length, wemb_dim]
-        q_emb = self.q_emb(w_emb)  # question representation [2 * batch, q_dim]
-
-        att = self.v_att(v, q_emb)  # attention weight [2 * batch, num_objs, obj_dim]
-        v_emb = (att * v).sum(1)  # attended feature vector [2 * batch, obj_dim]
-
-        q_repr = self.q_net(q_emb)  # question representation [2 * batch, num_hid]
-        v_repr = self.v_net(v_emb)  # image representation [2 * batch, num_hid]
-        joint_repr = q_repr * v_repr  # joint embedding (joint representation) [2 * batch, num_hid]
-        logits = self.classifier(joint_repr)  # answer (answer probabilities) [2 * batch, n_answers]
         return logits
 
 def build_baseline0(dataset, num_hid):
