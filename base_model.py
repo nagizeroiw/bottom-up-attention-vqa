468e335a78a4eaee970983166588efecfa6a69c5
nagizeroiw <nagizero@foxmail.com>
2018-04-02 10:35:59 +0800

    minor

--- base_model.py
+++ base_model.py
@@ -19,12 +19,17 @@ class BaseModel(nn.Module):
 
         self.seen_back2normal_shape = True
 
+    def see(self, var, name):
+        if not self.seen_back2normal_shape:
+            print('name', var.size())
+
     def forward(self, v, b, q, labels):
         """Forward
 
         v: [batch, 2, num_objs, obj_dim]
         b: [batch, 2, num_objs, b_dim]
         q: [batch, 2, seq_length]
+        labels: [batch, 2, n_ans]
 
         return: logits, not probs
         """
@@ -59,29 +64,22 @@ class BaseModel(nn.Module):
         v_repr = self.v_net(v_emb)  # image representation [2 * batch, num_hid]
         joint_repr = q_repr * v_repr  # joint embedding (joint representation) [2 * batch, num_hid]
 
+
+        logits = self.classifier(joint_repr)  # answer (answer probabilities) [2 * batch, n_answers]
+
         if with_pair_loss:
             '''
-            # pair_loss_1
+            ### pair_loss_1 (on joint representations)
             joint_repr = joint_repr.view(batch, 2, -1)  # [batch, 2, num_hid]
-            if not self.seen_back2normal_shape:
-                print('joint_repr', joint_repr.size())
-
             joint_repr = joint_repr.transpose(1, 2)  # [batch, num_hid, 2]
-            if not self.seen_back2normal_shape:
-                print('joint_repr', joint_repr.size())
-
             repr1, repr2 = joint_repr[:, :, 0], joint_repr[:, :, 1]  # [batch, num_hid] * 2
-            if not self.seen_back2normal_shape:
-                print('repr1|repr2', repr1.size())
 
             joint_repr = joint_repr.transpose(1, 2).view(batch * 2, -1)  # [2 * batch, num_hid]
-
             pair_loss = -1. * self.pair_loss_weight * (repr1 - repr2).norm(dim=1)  # [batch,]
-            if not self.seen_back2normal_shape:
-                print('pair_loss', pair_loss.size())
             '''
 
-            # pair_loss_2
+            '''
+            ### pair_loss_2 (on attended image features)
             v_emb = v_emb.view(batch, 2, -1)  # [batch, 2, obj_dim]
             v_emb = v_emb.transpose(1, 2)  # [batch, obj_dim, 2]
             emb1, emb2 = v_emb[:, :, 0], v_emb[:, :, 1]  # [batch, obj_dim] * 2
@@ -91,8 +89,50 @@ class BaseModel(nn.Module):
             pair_loss = pair_loss.mean(dim=0, keepdim=True) # [1,]
 
             self.seen_back2normal_shape = True
+            '''
 
-        logits = self.classifier(joint_repr)  # answer (answer probabilities) [2 * batch, n_answers]
+            ### pair_loss_3 (max_margin)
+            logits = logits.view(batch, 2, -1)  # [batch, 2, n_ans]
+            logits1, logits2 = logits[:, 0, :], logits[:, 1, :]  # [batch, n_ans] * 2
+            self.see(logits1, 'logits1')
+            self.see(logits2, 'logits2')
+            labels1, labels2 = labels[:, 0, :], labels[:, 1, :]  # [batch, n_ans] * 2
+            self.see(labels1, 'labels1')
+            self.see(labels2, 'labels2')
+            v_emb = v_emb.view(batch, 2, -1)  # [batch, 2, obj_dim]
+            v1, v2 = v_emb[:, 0, :], v_emb[:, 1, :]
+            self.see(v1, 'v1')
+            self.see(v2, 'v2')
+
+            f_2_1 = logits1 * labels2 - logits1 * labels1
+            f_1_2 = logits2 * labels1 - logits2 * labels2
+            self.see(f_2_1, 'f_2_1')
+            self.see(f_1_2, 'f_1_2')
+
+            logits1.backward(labels2)
+            df2_1 = v1.grad  # shape?
+            self.see(df2_1, 'df2_1')
+            self.zero_grad()
+            logits1.backward(labels1)
+            df1_1 = v1.grad
+            self.see(df1_1, 'df1_1')
+            self.zero_grad()
+            logits2.backward(labels1)
+            df1_2 = v2.grad
+            self.see(df1_2, 'df1_2')
+            self.zero_grad()
+            logits2.backward(labels2)
+            df2_2 = v2.grad
+            self.see(df2_2, 'df2_2')
+
+            ploss_1 = f_2_1 / (df2_1 - df1_1).norm(dim=1)
+            ploss_2 = f_1_2 / (df1_2 - df2_2).norm(dim=1)
+            self.see(ploss_1, 'ploss_1')
+            self.see(ploss_2, 'ploss_2')
+
+            pair_loss = (ploss_1 + ploss_2).mean(keepdim=True)
+            print(pair_loss)
+
         if with_pair_loss:
             return logits, pair_loss, raw_pair_loss
         return logits, None, None
