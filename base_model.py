cc42f501758307ed22d46b284e9aa5003868eee9
nagizero <nagizero@foxmail.com>
2018-04-03 12:51:27 +0800

    minor

--- base_model.py
+++ base_model.py
@@ -18,6 +18,7 @@ class BaseModel(nn.Module):
         self.v_net = v_net
         self.classifier = classifier
         self.pair_loss_weight = args.pair_loss_weight
+        self.pair_loss_type = args.pair_loss_type
 
         self.seen_back2normal_shape = False
 
@@ -72,83 +73,99 @@ class BaseModel(nn.Module):
         if with_pair_loss:
             '''
             ### pair_loss_1 (on joint representations)
-            joint_repr = joint_repr.view(batch, 2, -1)  # [batch, 2, num_hid]
-            joint_repr = joint_repr.transpose(1, 2)  # [batch, num_hid, 2]
-            repr1, repr2 = joint_repr[:, :, 0], joint_repr[:, :, 1]  # [batch, num_hid] * 2
-
-            joint_repr = joint_repr.transpose(1, 2).view(batch * 2, -1)  # [2 * batch, num_hid]
-            pair_loss = -1. * self.pair_loss_weight * (repr1 - repr2).norm(dim=1)  # [batch,]
+            
             '''
 
             '''
             ### pair_loss_2 (on attended image features)
-            v_emb = v_emb.view(batch, 2, -1)  # [batch, 2, obj_dim]
-            v_emb = v_emb.transpose(1, 2)  # [batch, obj_dim, 2]
-            emb1, emb2 = v_emb[:, :, 0], v_emb[:, :, 1]  # [batch, obj_dim] * 2
-
-            raw_pair_loss = (emb1 - emb2).norm(dim=1) # [batch,]
-            pair_loss = -1. * self.pair_loss_weight * raw_pair_loss  # [batch,]            
-            pair_loss = pair_loss.mean(dim=0, keepdim=True) # [1,]
-
-            self.seen_back2normal_shape = True
+            
             '''
 
             ### pair_loss_3 (max_margin)
-            labels1, labels2 = labels[:, 0, :], labels[:, 1, :]  # [batch, n_ans] * 2
+            if self.pair_loss_type == '@att':
+                v_emb = v_emb.view(batch, 2, -1)  # [batch, 2, obj_dim]
+                v_emb = v_emb.transpose(1, 2)  # [batch, obj_dim, 2]
+                emb1, emb2 = v_emb[:, :, 0], v_emb[:, :, 1]  # [batch, obj_dim] * 2
 
-            logits = logits.view(batch, 2, -1)  # [batch, 2, n_ans]
-            logits1, logits2 = logits[:, 0, :], logits[:, 1, :]  # [batch, n_ans] * 2
+                raw_pair_loss = (emb1 - emb2).norm(2, dim=1) # [batch,]
+                pair_loss = -1. * self.pair_loss_weight * raw_pair_loss  # [batch,]            
+                pair_loss = pair_loss.mean(dim=0, keepdim=False) # [,]
+                raw_pair_loss = raw_pair_loss.mean(dim=0, keepdim=False)
 
-            logits1.backward(labels2, retain_graph=True)
-            df2_1 = v_emb.grad  # [batch * 2, v_dim]
-            df2_1 = df2_1.view(batch, 2, -1)[:, 0, :]  # [batch, v_dim]
-            # print('df2_1', df2_1.size())
-            self.zero_grad()
+                self.seen_back2normal_shape = True
 
-            logits1.backward(labels1, retain_graph=True)
-            df1_1 = v_emb.grad  # [batch * 2, v_dim]
-            df1_1 = df1_1.view(batch, 2, -1)[:, 0, :]  # [batch, v_dim]
-            # print('df1_1', df1_1.size())
-            self.zero_grad()
+            elif self.pair_loss_type == '@repr':
+                joint_repr = joint_repr.view(batch, 2, -1)  # [batch, 2, num_hid]
+                joint_repr = joint_repr.transpose(1, 2)  # [batch, num_hid, 2]
+                repr1, repr2 = joint_repr[:, :, 0], joint_repr[:, :, 1]  # [batch, num_hid] * 2
 
-            logits2.backward(labels1, retain_graph=True)
-            df1_2 = v_emb.grad  # [batch * 2, v_dim]
-            df1_2 = df1_2.view(batch, 2, -1)[:, 1, :]  # [batch, v_dim]
-            # print('df1_2', df2_1.size())
-            self.zero_grad()
+                joint_repr = joint_repr.transpose(1, 2).view(batch * 2, -1)  # [2 * batch, num_hid]
+                raw_pair_loss = (repr1 - repr2).norm(2, dim=1)
+                pair_loss = -1. * self.pair_loss_weight * raw_pair_loss  # [,]
+                pair_loss = pair_loss.mean(dim=0, keepdim=False)
 
-            logits2.backward(labels2, retain_graph=True)
-            df2_2 = v_emb.grad  # [batch * 2, v_dim]
-            df2_2 = df2_2.view(batch, 2, -1)[:, 1, :]  # [batch, v_dim]
-            # print('df2_2', df2_1.size())
-            self.zero_grad()
+                self.seen_back2normal_shape = True
 
-            # ~ 1e-3
-            # print(torch.max(df2_1), torch.max(df1_1), torch.max(df1_2), torch.max(df2_2))
+            elif self.pair_loss_type == 'margin':
+                labels1, labels2 = labels[:, 0, :], labels[:, 1, :]  # [batch, n_ans] * 2
 
-            logits = logits.view(batch * 2, -1)  # [batch * 2, n_ans]
+                logits = logits.view(batch, 2, -1)  # [batch, 2, n_ans]
+                logits1, logits2 = logits[:, 0, :], logits[:, 1, :]  # [batch, n_ans] * 2
 
-            ################################################################################
+                logits1.backward(labels2, retain_graph=True)
+                df2_1 = v_emb.grad  # [batch * 2, v_dim]
+                df2_1 = df2_1.view(batch, 2, -1)[:, 0, :]  # [batch, v_dim]
+                # print('df2_1', df2_1.size())
+                v_emb.grad.zero_()
+                self.zero_grad()
 
-            f_2_1 = logits1 * labels2 - logits1 * labels1  # [batch, n_ans]
-            f_1_2 = logits2 * labels1 - logits2 * labels2  # [batch, n_ans]
+                logits1.backward(labels1, retain_graph=True)
+                df1_1 = v_emb.grad  # [batch * 2, v_dim]
+                df1_1 = df1_1.view(batch, 2, -1)[:, 0, :]  # [batch, v_dim]
+                # print('df1_1', df1_1.size())
+                v_emb.grad.zero_()
+                self.zero_grad()
 
-            # ~ 1e-3
-            # print(f_2_1.sum(dim=1).mean(dim=0).item())
+                logits2.backward(labels1, retain_graph=True)
+                df1_2 = v_emb.grad  # [batch * 2, v_dim]
+                df1_2 = df1_2.view(batch, 2, -1)[:, 1, :]  # [batch, v_dim]
+                # print('df1_2', df2_1.size())
+                v_emb.grad.zero_()
+                self.zero_grad()
 
-            # ~ 5e-3
-            # print((df2_1 - df1_1).norm(2, dim=1).mean(dim=0).item())
+                logits2.backward(labels2, retain_graph=True)
+                df2_2 = v_emb.grad  # [batch * 2, v_dim]
+                df2_2 = df2_2.view(batch, 2, -1)[:, 1, :]  # [batch, v_dim]
+                # print('df2_2', df2_1.size())
+                v_emb.grad.zero_()
+                self.zero_grad()
 
-            pair_loss_1 = f_2_1.sum(dim=1) / ((df2_1 - df1_1).norm(2, dim=1) + 1e-8)  # [batch,]
-            pair_loss_2 = f_1_2.sum(dim=1) / ((df1_2 - df2_2).norm(2, dim=1) + 1e-8)  # [batch,]
+                # ~ 1e-3
+                # print(torch.max(df2_1), torch.max(df1_1), torch.max(df1_2), torch.max(df2_2))
 
-            pair_loss_1 = pair_loss_1.clamp(-50., 50.)
-            pair_loss_2 = pair_loss_2.clamp(-50., 50.)
-            # print(pair_loss_1.mean().item())
+                logits = logits.view(batch * 2, -1)  # [batch * 2, n_ans]
 
-            self.seen_back2normal_shape = True
-            raw_pair_loss = (pair_loss_1).mean()
-            pair_loss = self.pair_loss_weight * raw_pair_loss
+                ################################################################################
+
+                f_2_1 = logits1 * labels2 - logits1 * labels1  # [batch, n_ans]
+                f_1_2 = logits2 * labels1 - logits2 * labels2  # [batch, n_ans]
+
+                # ~ 1e-3
+                # print(f_2_1.sum(dim=1).mean(dim=0).item())
+
+                # ~ 5e-3
+                # print((df2_1 - df1_1).norm(2, dim=1).mean(dim=0).item())
+
+                pair_loss_1 = f_2_1.sum(dim=1) / ((df2_1 - df1_1).norm(2, dim=1) + 1e-8)  # [batch,]
+                pair_loss_2 = f_1_2.sum(dim=1) / ((df1_2 - df2_2).norm(2, dim=1) + 1e-8)  # [batch,]
+
+                pair_loss_1 = pair_loss_1.clamp(-50., 50.)
+                pair_loss_2 = pair_loss_2.clamp(-50., 50.)
+                # print(pair_loss_1.mean().item())
+
+                self.seen_back2normal_shape = True
+                raw_pair_loss = (pair_loss_1).mean()
+                pair_loss = self.pair_loss_weight * raw_pair_loss
 
         if with_pair_loss:
             return logits, pair_loss, raw_pair_loss
