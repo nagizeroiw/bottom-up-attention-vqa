417f8be0f4c4c6545a54e46cbde1f70a99e88aac
nagizeroiw <nagizero@foxmail.com>
2018-04-04 18:22:02 +0800

    minor

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ rm -r saved_models/$ID
 CUDA_VISIBLE_DEVICES=1 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
-    --pair_loss_weight 0.1 \
+    --pair_loss_weight 0.01 \
     --pair_loss_type margin
