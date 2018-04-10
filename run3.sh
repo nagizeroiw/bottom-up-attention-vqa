4d0dc87f93ffd672a2b724ad97586754063999b6
nagizeroiw <nagizero@foxmail.com>
2018-04-10 11:10:50 +0800

    minor

--- run3.sh
+++ run3.sh
@@ -1,6 +1,6 @@
 ID=test_pair_loss_4_cl1
 rm -r saved_models/$ID
-CUDA_VISIBLE_DEVICES=2 python main.py \
+CUDA_VISIBLE_DEVICES=3 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
     --pair_loss_weight 0 \
