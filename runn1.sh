82707cc6ae384bbed4fe604c8962149f1d67a1d7
nagizeroiw <nagizero@foxmail.com>
2018-04-16 14:37:44 +0800

    minor

--- runn1.sh
+++ runn1.sh
@@ -1,8 +1,8 @@
-ID=test_pair_loss_4
+ID=test_pair_loss_3_back
 rm -r saved_models/$ID
 CUDA_VISIBLE_DEVICES=1 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
     --pair_loss_weight 0 \
-    --pair_loss_type margin@repr \
+    --pair_loss_type @att \
     --gamma 2.5
--- runn2.sh
+++ runn2.sh
@@ -1,8 +1,8 @@
-ID=pair_loss_4_g2.5
+ID=pair_loss_3_w0.05
 rm -r saved_models/$ID
 CUDA_VISIBLE_DEVICES=2 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
     --pair_loss_weight 0.05 \
-    --pair_loss_type margin@repr \
+    --pair_loss_type @att \
     --gamma 2.5
