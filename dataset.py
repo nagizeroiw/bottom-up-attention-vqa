faf34dea0d647912eae4700bfe266c6d6d0b4849
nagizeroiw <nagizero@foxmail.com>
2018-04-27 16:32:50 +0800

    minor

--- dataset.py
+++ dataset.py
@@ -390,6 +390,6 @@ if __name__ == '__main__':
     dataset = VQAFeatureDatasetEnd2End('train', dictionary)
     print(dataset[0])
     print(dataset[199])
-    dataset = VQAFeatureDatasetEnd2End('valid', dictionary)
+    dataset = VQAFeatureDatasetEnd2End('val', dictionary)
     print(dataset[0])
     print(dataset[199])
