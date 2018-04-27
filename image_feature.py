a112f9c2b51ce46a0a13253631aae333ab022afd
nagizero <nagizero@foxmail.com>
2018-04-27 10:25:38 +0800

    -

--- image_feature.py
+++ image_feature.py
@@ -5,6 +5,20 @@ import os
 import h5py
 import cPickle
 
+'''
+get_all_images():
+    from downloaded COCO dataset to pickle files storing 
+        split_train.pkl
+        split_valid.pkl
+        split_test.pkl
+            {
+                'image_ids': [0, ...]
+                'file_names': ['data/images/COCO_train2014_000000000000.jpg', ...]
+            }
+
+
+'''
+
 DATAROOT = 'data/images/'
 folder = {
     'train': 'train2014/',
@@ -16,6 +30,11 @@ prefix = {
     'valid': 'COCO_val2014_000000',
     'test': 'COCO_test2014_000000',
 }
+dataset_output_file = {
+    'train': 'split_train.pkl',
+    'valid': 'split_valid.pkl',
+    'test': 'split_test.pkl'
+}
 
 
 def get_images(split):
@@ -25,16 +44,22 @@ def get_images(split):
     for parent, dirnames, filenames in os.walk(directory):
         for filename in filenames:
             if os.path.isfile(os.path.join(directory, filename)):
-                print(filename, ':', int(filename[-6:]))
-                img_ids.append(int(filename[-6:]))
+                img_id = int(filename.split('.')[0][-6:])
+                # print(filename, ':', img_id)
+                img_ids.append(img_id)
                 filenames.append(os.path.join(directory, filename))
-    return img_ids, filenames
+    print('> find %d images for split %s.' % (len(img_ids), split))
+    to_save = {
+        'image_ids': img_ids,
+        'file_names': filenames
+    }
+    cPickle.dump(to_save, open(os.path.join(DATAROOT, dataset_output_file[split]), 'w'))
 
 
 def get_all_images():
-    train_ids, train_files = get_images('train')
-    valid_ids, valid_files = get_images('valid')
-    test_ids, test_files = get_images('test')
+    get_images('train')
+    get_images('valid')
+    get_images('test')
 
 
 if __name__ == '__main__':
