f51b45d2c63ad2a96d2138015e246ea4a6a701a1
nagizeroiw <nagizero@foxmail.com>
2018-05-07 09:49:47 +0800

    minor

--- runn2.sh
+++
@@ -1,9 +0,0 @@
-ID=test_pair_loss_4_back_g1.0
-rm -r saved_models/$ID
-CUDA_VISIBLE_DEVICES=2 python main.py \
-    --output saved_models/$ID \
-    --epochs 40 \
-    --pair_loss_weight 0.05 \
-    --pair_loss_type margin@repr \
-    --gamma 1 \
-    --seed 5293
