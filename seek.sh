7e511cc4ae33de6bf4ffe6b550f5a64bbd812403
nagizeroiw <nagizero@foxmail.com>
2018-05-09 16:07:17 +0800

    minor

--- seek.sh
+++ seek.sh
@@ -33,7 +33,7 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --model dualatt \
     --task seek-val \
     --seek_qid $QID2 \
-    --seek_output $QID2_pairwise.png
+    --seek_output $QID2_pairwise.png \
     --start_with saved_models/dualatt_pairwise \
     --seed 5293
 
@@ -42,7 +42,7 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --model dualatt \
     --task seek-val \
     --seek_qid $QID1 \
-    --seek_output $QID1_pairwise.png
+    --seek_output $QID1_pairwise.png \
     --start_with saved_models/dualatt_ploss \
     --seed 5293
     
@@ -51,7 +51,7 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --model dualatt \
     --task seek-val \
     --seek_qid $QID2 \
-    --seek_output $QID2_pairwise.png
+    --seek_output $QID2_pairwise.png \
     --start_with saved_models/dualatt_ploss \
     --seed 5293
     
