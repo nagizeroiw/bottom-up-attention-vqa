26d1484e2eaae7ae03644f90a213d08980206509
nagizero <nagizero@foxmail.com>
2018-05-09 15:52:42 +0800

    minor

--- seek.sh
+++ seek.sh
@@ -2,5 +2,6 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --batch_size 256 \
     --model dualatt \
     --task seek-val \
-    --start_with saved_models/dualatt_ploss \
+    --seek_qid 267225012 \
+    --start_with saved_models/dualatt_filter \
     --seed 5293
