8ca50d41d435875cdc5b614e5f8b5b48d0eaa868
nagizero <nagizero@foxmail.com>
2018-05-09 15:13:46 +0800

    minor

--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
-ID=stackatt2_all
+ID=stackdualatt2_all
 rm -r saved_models/$ID
-CUDA_VISIBLE_DEVICES=2 python main.py \
+CUDA_VISIBLE_DEVICES=3 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
     --pair_loss_weight 0 \
