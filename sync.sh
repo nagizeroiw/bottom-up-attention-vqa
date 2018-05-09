99b1dd5cd329d707e460dee04608f9debfaf43ef
nagizeroiw <nagizero@foxmail.com>
2018-05-09 09:33:57 +0800

    minor

--- sync.sh
+++ sync.sh
@@ -1,7 +1,3 @@
-ID1=dualatt_2layer
-ID2=dualatt_no_pair
-ID3=dualatt_1280
+ID1=dualatt_all_pair
 scp -r jungpu6:~/vqa-butd/saved_models/$ID1/tf_log ./tf_log/$ID1
-scp -r jungpu6:~/vqa-butd/saved_models/$ID2/tf_log ./tf_log/$ID2
-scp -r jungpu6:~/vqa-butd/saved_models/$ID3/tf_log ./tf_log/$ID3
 tensorboard --logdir=./tf_log/
