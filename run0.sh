abe473ee6b997a4d136f214b466b0eadf57ca292
nagizeroiw <nagizero@foxmail.com>
2018-05-16 16:17:30 +0800

    minor

--- run0.sh
+++ run0.sh
@@ -1,12 +1,12 @@
-ID=dualatt_clean_ploss
+ID=ens_5293
 rm -r saved_models/$ID
 CUDA_VISIBLE_DEVICES=2 python main.py \
     --output saved_models/$ID \
     --epochs 40 \
     --pair_loss_weight 0.05 \
-    --pair_loss_type margin@repr \
+    --pair_loss_type @att \
     --gamma 2.5 \
     --model dualatt \
-    --train_dataset pairwise \
+    --train_dataset all \
     --test_dataset all \
     --seed 5293
