4a5fabeba75549212c0782e9e35146ea24e15b45
nagizeroiw <nagizero@foxmail.com>
2018-05-25 16:14:44 +0800

    minor

--- seek_a_lot.sh
+++ seek_a_lot.sh
@@ -5,7 +5,6 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --batch_size 256 \
     --model dualatt \
     --task seek-val \
-    --seek_qid $QID1 \
     --seek_output './fig_sal' \
     --start_with saved_models/dualatt_filter \
     --seed 5293
