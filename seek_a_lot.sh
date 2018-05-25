7b1c759ff820a9bc18db94137c102aca52bfc258
nagizeroiw <nagizero@foxmail.com>
2018-05-26 01:21:19 +0800

    minor

--- seek_a_lot.sh
+++ seek_a_lot.sh
@@ -8,3 +8,19 @@ CUDA_VISIBLE_DEVICES=3 python main.py \
     --seek_output './fig_sal' \
     --start_with saved_models/dualatt_filter \
     --seed 5293
+
+CUDA_VISIBLE_DEVICES=3 python main.py \
+    --batch_size 256 \
+    --model dualatt \
+    --task seek-val \
+    --seek_output './fig_sal' \
+    --start_with saved_models/dualatt_pairwise \
+    --seed 5293
+
+CUDA_VISIBLE_DEVICES=3 python main.py \
+    --batch_size 256 \
+    --model dualatt \
+    --task seek-val \
+    --seek_output './fig_sal' \
+    --start_with saved_models/dualatt_ploss \
+    --seed 5293
